0af6d6449e1ddcf6e392e0ed4221d5f90b41cefd
bpoche <bpoche@vt.edu>
2018-03-05 12:07:04 +0000

    added logic to handle imu baudrate exceptions

--- tracking/ism_simulate.py
+++ tracking/ism_simulate.py
@@ -62,16 +62,21 @@ def stop_video(vid_data_loc):
     cam.command('record','00')
     print('stopping video')
     time.sleep(delay)
-    gpCam = GoProCamera.GoPro(constants.auth)
-    time.sleep(delay+2)
-    #print('obtaining filename (unobtanium?)',vid_data_loc)
-    file_name=gpCam.getMedia().split('videos')[1]
-    time.sleep(2)
-    vid_data = pd.read_csv(vid_data_loc,index_col='run')
-    vid_data.loc[vid_data.index[0],'t_stop']=datetime.now()
-    vid_data.loc[vid_data.index[0],'filename']=file_name
-    vid_data.to_csv(vid_data_loc,index_label='run')
-    print('filename = ',file_name)
+    try:
+        gpCam = GoProCamera.GoPro(constants.auth)
+        time.sleep(delay+2)
+        #print('obtaining filename (unobtanium?)',vid_data_loc)
+        file_name=gpCam.getMedia().split('videos')[1]
+        time.sleep(2)
+        vid_data = pd.read_csv(vid_data_loc,index_col='run')
+        vid_data.loc[vid_data.index[0],'t_stop']=datetime.now()
+        vid_data.loc[vid_data.index[0],'filename']=file_name
+        vid_data.to_csv(vid_data_loc,index_label='run')
+        print('filename = ',file_name)
+    except:
+        print('')
+        print('******FAILED to get gopro video filename, check wifi connection')
+        print('')
     time.sleep(2)
     return
 
@@ -101,70 +106,96 @@ def rec_imu_async(imu_com_port,imu_baudrate,save_loc,file_prefix,loop_trigger,im
     print('rec_imu_async starting')
     
     try:
-        ez = EzAsyncData.connect(imu_com_port, imu_baudrate)
-        if ez.sensor.read_async_data_output_frequency()!=imu_freq:
-            ez.sensor.write_async_data_output_frequency(imu_freq)
-            
+        ez = EzAsyncData.connect(imu_com_port,imu_baudrate)
+        if (imu_freq > 40) & (imu_baudrate < 921600):
+            print('Insufficient baud rate for desired imu_freq of ',imu_freq,' hz, setting baudrate to 921600')
+            ez.sensor.change_baudrate(921600)
+        #ez = EzAsyncData.connect(imu_com_port,imu_baudrate)
+        ez.sensor.write_async_data_output_frequency(imu_freq)
+        print('imu baudrate =',ez.sensor.baudrate)
+        print('imu frequency =',ez.sensor.read_async_data_output_frequency())
     except:
         print('Houston, got an imu issue, please consult the oracle')
-        
+    ypr = []
+    accel=[]
+    ang_r=[]
+    mag=[]
+    timestamp=[]
     t0 = time.time()
-    data = pd.DataFrame(columns=['elasped',
-                                 'yaw',
-                                 'pitch',
-                                 'roll',
-                                 'mag_x',
-                                 'mag_y',
-                                 'mag_z',
-                                 'accel_x',
-                                 'accel_y',
-                                 'accel_z',
-                                 'ang_x',
-                                 'ang_y',
-                                 'ang_z'])
-    print('imu_rec_asyc loop_trigger=',loop_trigger.value)
+    interval = 1/float(imu_freq)
+    time.sleep(.5)
     while loop_trigger.value==1:
         #print('rec_imu loop',loop_trigger.value)
-        t1=time.time()
         try:
-            yaw = ez.current_data.yaw_pitch_roll.x
-            pitch = ez.current_data.yaw_pitch_roll.y
-            roll = ez.current_data.yaw_pitch_roll.z
-            mag_x = ez.current_data.magnetic.x
-            mag_y = ez.current_data.magnetic.y
-            mag_z = ez.current_data.magnetic.z
-            accel_x = ez.current_data.acceleration.x
-            accel_y = ez.current_data.acceleration.y
-            accel_z = ez.current_data.acceleration.z
-            ang_x = ez.current_data.angular_rate.x
-            ang_y = ez.current_data.angular_rate.y
-            ang_z = ez.current_data.angular_rate.z
+            t1=time.time()
+            ypr.append(ez.current_data.yaw_pitch_roll)
+            accel.append(ez.current_data.acceleration)
+            ang_r.append(ez.current_data.angular_rate)
+            mag.append(ez.current_data.magnetic)
+            timestamp.append(datetime.now())
+            dt=time.time()-t1
+            time.sleep(interval-dt)
         except:
-            yaw = np.nan
-            pitch = np.nan
-            roll = np.nan
-            mag_x = np.nan
-            mag_y = np.nan
-            mag_z = np.nan
-            accel_x = np.nan
-            accel_y = np.nan
-            accel_z = np.nan
-            ang_x = np.nan
-            ang_y = np.nan
-            ang_z = np.nan    
-        data.loc[datetime.now()] = [round(1000*(t1-t0),4),
-                                    yaw,
-                                    pitch,
-                                    roll,
-                                    mag_x,
-                                    mag_y,
-                                    mag_z,
-                                    accel_x,
-                                    accel_y,
-                                    accel_z,
-                                    ang_x,
-                                    ang_y,
-                                    ang_z]
+            ypr.append(np.nan)
+            accel.append(np.nan)
+            ang_r.append(np.nan)
+            mag.append(np.nan)
+            timestamp.append(datetime.now())
+            
+    yaw=[]
+    pitch=[]
+    roll=[]
+    accel_x=[]
+    accel_y=[]
+    accel_z=[]
+    ang_x=[]
+    ang_y=[]
+    ang_z=[]
+    mag_x=[]
+    mag_y=[]
+    mag_z=[]
+    for i in range(len(ypr)):
+        try:
+            yaw.append(ypr[i].x)
+            pitch.append(ypr[i].y)
+            roll.append(ypr[i].z)
+            accel_x.append(accel[i].x)
+            accel_y.append(accel[i].y)
+            accel_z.append(accel[i].z)
+            ang_x.append(ang_r[i].x)
+            ang_y.append(ang_r[i].y)
+            ang_z.append(ang_r[i].z)
+            mag_x.append(mag[i].x)
+            mag_y.append(mag[i].y)
+            mag_z.append(mag[i].z)            
+        except:
+            yaw.append(np.nan)
+            pitch.append(np.nan)
+            roll.append(np.nan)
+            accel_x.append(np.nan)
+            accel_y.append(np.nan)
+            accel_z.append(np.nan)
+            ang_x.append(np.nan)
+            ang_y.append(np.nan)
+            ang_z.append(np.nan)
+            mag_x.append(np.nan)
+            mag_y.append(np.nan)
+            mag_z.append(np.nan) 
+            
+    data = pd.DataFrame({'time':timestamp,
+                         'yaw':yaw,
+                         'pitch':pitch,
+                         'roll':roll,
+                         'accel_x':accel_x,
+                         'accel_y':accel_y,
+                         'accel_z':accel_z,
+                         'ang_x':ang_x,
+                         'ang_y':ang_y,
+                         'ang_z':ang_z,
+                         'mag_x':mag_x,
+                         'mag_y':mag_y,
+                         'mag_z':mag_z})
+    data = data.set_index('time')
     data.to_csv(save_loc+'/vn100_'+file_prefix+'.csv',
                 index_label='time')
     print('rec_imu async done')
@@ -399,7 +430,12 @@ def ptu_simulate(ptu_ebay_com_port,
     return
  
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='1. Start Video 2. Record IMU 3. Simulate PTU 4. Shutter Down')
+    parser = argparse.ArgumentParser(description='1. Start Video 2. Record IMU 3. Simulate PTU 4. Shutter Down\n'+
+                                     'If imu not working, ensure ensure baudrate, com port and imu_freq are correct\n'+
+                                     '>>>from vnpy import *\n'+
+                                     ">>>ez = EzAsyncData.connect('/dev/ttyUSB0', 115200)   #test com port/baud rate with putty"+
+                                     ">>>ez.sensor.change_baudrate(921600)\n"+
+                                     ">>>ez.sensor.write_async_data_output_frequency(100)  #or whatever imu_freq you want")
     ########### GoPro parameters ################
     parser.add_argument('-gp_p','--gp_passw',
                         default='',
@@ -434,7 +470,7 @@ if __name__ == '__main__':
 ########### IMU parameters ################ 
     
     parser.add_argument('-imu_c','--imu_com_port',
-                        default='/dev/ttyUSB1',
+                        default='/dev/ttyUSB0',
                         type=str,
                         help='com port name')
     parser.add_argument('-imu_b','--imu_baudrate',
@@ -509,8 +545,11 @@ if __name__ == '__main__':
     
     #Start loop to execute every simulation file in the params.ptu_sim_dir provided
     for i in range(len(ptu_sim_files)):
-        
+        print('')
+        print('#######################################################################')
         print('running sim_file',ptu_sim_files[i])
+        print('#######################################################################')
+        print('')
         loop_trigger.value=1       #reset loop_trigger to 1 to enable imu_recording
         run_num=run_num+i     
         os.makedirs(params.save_loc+'run_'+str(run_num))  #create new folder for each simulation run
@@ -557,22 +596,16 @@ if __name__ == '__main__':
         while ptu_sim.is_alive():
             pass
         print('bout to turn off loop tirgger',loop_trigger.value)
-        print('done turnt off the loop trigger',loop_trigger.value)
+        #print('done turnt off the loop trigger',loop_trigger.value)
+        loop_trigger.value=0
         time.sleep(1)
-        #imu_record.terminate()
-        #ptu_sim.terminate()
         gopro_stop.start()
         while gopro_stop.is_alive():
-            pass
-        loop_trigger.value=0
-        time.sleep(5)
+            pass    
+        time.sleep(2)
         vid_data = pd.read_csv(vid_data_loc,index_col='run')
-        print('End of simulation, thanks for playing!')
-    #gopro_stop.terminate()
-    #gopro_stop.join()
-    #imu_record.join()
-    #ptu_ebay.join()
-    #exit()
+    print('End of simulation, thanks for playing!')
+
 
 ##    print('path to save',params.save_loc+file_prefix.split('.csv')[0]+'_vid_filenames.txt')
 ##    with open(params.save_loc+file_prefix.split('.csv')[0]+'_vid_filenames.txt','w') as f:
